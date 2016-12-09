b2c6a8c1b436a46ee00e4e931ca260e459fd5585
samanthaksanders <samantha.k.sanders@gmail.com>
2016-12-10 12:05:57 +1300

    Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+girls = ['Sam', 'Laura', 'Hannah', 'Rachel']
+
+def hi(girl):
+    print('Hi ' + girl + '!')
+
+for name in girls:
+	hi(name)
+	print('Next girl!')
+
+for i in range(1, 10):
+	print(i)
